d5de7a51191b02ce7eefd100ad6602966293b35c
montd <dzejdzej.j@gmail.com>
2021-10-07 23:55:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+
+def main():
+    URL = "https://www.olx.pl/nieruchomosci/gdansk/?search%5Bfilter_float_price%3Afrom%5D=400&search%5Bfilter_float_price%3Ato%5D=1200&search%5Bprivate_business%5D=private&search%5Bdistrict_id%5D=135&reason=observed_search"
+    page = get(URL)
+    bs = BeautifulSoup(page.content)
+
+if __name__ == "__main__":
+    main()
