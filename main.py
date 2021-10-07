526862e0849161a918b0bb2703dff8a14e2358d8
montd <dzejdzej.j@gmail.com>
2021-10-08 00:25:43 +0200

    Koszt

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from requests import get
 def main():
     URL = "https://www.olx.pl/nieruchomosci/gdansk/?search%5Bfilter_float_price%3Afrom%5D=400&search%5Bfilter_float_price%3Ato%5D=1200&search%5Bprivate_business%5D=private&search%5Bdistrict_id%5D=135&reason=observed_search"
     page = get(URL)
-    bs = BeautifulSoup(page.content)
+    bs = BeautifulSoup(page.content, features='html.parser')
 
-# test
+    for offer in bs.find_all('div', class_="offer-wrapper"):
+        koszt = offer.find('p', class_='price')
+        print(koszt)
+
 
 if __name__ == "__main__":
     main()
